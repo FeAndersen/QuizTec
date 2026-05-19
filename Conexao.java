8ee2a72eab363d25ce6b152efccb86a6e87ec43f
tiagohodas <hodastiago22@gmail.com>
2026-05-19 11:53:12 -0300

    adicionado conexao com banco de dados

---
+++ Conexao.java
@@ -0,0 +1,21 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+    private static final String URL = "jdbc:mysql://localhost:3306/quiztec";
+    private static final String USUARIO = "root";
+    private static final String SENHA = "clnh61357";
+
+    public static Connection conectar() {
+        try {
+            return DriverManager.getConnection(URL, USUARIO, SENHA);
+        } catch (SQLException e) {
+            System.out.println("Erro ao conectar com o banco: " + e.getMessage());
+            return null;
+        }
+    }
+}
+
+
