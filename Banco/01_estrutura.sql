9f599388a1e4c8dc7fc99918cbb1822e7f07b066
tiagohodas <hodastiago22@gmail.com>
2026-05-19 11:43:42 -0300

    banco de dados adicionado

---
+++ Banco/01_estrutura.sql
@@ -0,0 +1,171 @@
+-- ativando banco de dados
+DROP SCHEMA IF EXISTS quiztec;
+CREATE SCHEMA quiztec;
+USE quiztec;
+-- SELECT @@default_storage_engine; usado durante desenvolvimento para verificar engine padrão
+-- criando tabela professor
+CREATE TABLE professor (
+id_professor INT AUTO_INCREMENT PRIMARY KEY,
+nome_professor VARCHAR(100) NOT NULL,
+email_professor VARCHAR(255) NOT NULL UNIQUE,
+senha_professor VARCHAR(255) NOT NULL
+);
+
+-- criando tabela imagem
+CREATE TABLE imagem (
+id_imagem INT AUTO_INCREMENT PRIMARY KEY,
+arquivo_imagem BLOB NOT NULL,
+tipo_imagem ENUM('jpg', 'jpeg', 'png', 'webp', 'gif') NOT NULL,
+nome_imagem VARCHAR(30) NOT NULL,
+descricao_imagem MEDIUMTEXT NULL
+);
+
+-- criando tabela dificuldade 
+CREATE TABLE dificuldade (
+id_dificuldade INT AUTO_INCREMENT PRIMARY KEY,
+nome_dificuldade VARCHAR(20) NOT NULL UNIQUE,
+ordem TINYINT UNSIGNED NOT NULL CHECK(ordem BETWEEN 1 AND 10),
+multiplicador_pontos DECIMAL(2,1) NOT NULL CHECK(multiplicador_pontos > 0)
+);
+
+-- criando tabela pergunta
+CREATE TABLE pergunta (
+id_pergunta INT AUTO_INCREMENT PRIMARY KEY,
+enunciado VARCHAR(250) NOT NULL,
+tipo_pergunta ENUM('material_funcao', 'funcao_material', 'material_sistema', 'sistema_material', 'identificacao') NOT NULL,
+pontuacao SMALLINT UNSIGNED NOT NULL CHECK(pontuacao > 0),
+ativo BOOLEAN NOT NULL DEFAULT TRUE,
+id_imagem INT NULL,
+FOREIGN KEY (id_imagem) REFERENCES imagem (id_imagem)
+			ON UPDATE CASCADE ON DELETE SET NULL
+);
+
+-- criando tabela sessao
+CREATE TABLE sessao (
+id_sessao INT AUTO_INCREMENT PRIMARY KEY,
+nome_sessao VARCHAR(30) NOT NULL,
+quantidade_perguntas TINYINT UNSIGNED NOT NULL CHECK(quantidade_perguntas > 0),
+data_criacao DATE NOT NULL DEFAULT (CURDATE()),
+ativa BOOLEAN NOT NULL DEFAULT TRUE,
+id_professor INT NOT NULL,
+id_dificuldade INT NOT NULL,
+FOREIGN KEY (id_professor) REFERENCES professor (id_professor)
+			ON UPDATE CASCADE ON DELETE RESTRICT,
+FOREIGN KEY (id_dificuldade) REFERENCES dificuldade (id_dificuldade)
+			ON UPDATE CASCADE ON DELETE RESTRICT,
+UNIQUE (nome_sessao, id_professor)
+);
+
+-- criando tabela turma
+CREATE TABLE turma (
+id_turma INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+serie TINYINT UNSIGNED NOT NULL CHECK(serie BETWEEN 1 AND 3),
+letra CHAR(1) NOT NULL CHECK(letra REGEXP '^[A-Z]$'),
+ano_letivo YEAR NOT NULL DEFAULT(YEAR(CURDATE())),
+id_professor INT NOT NULL,
+FOREIGN KEY (id_professor) REFERENCES professor (id_professor) 
+			ON DELETE RESTRICT ON UPDATE CASCADE,
+UNIQUE (serie, letra, ano_letivo, id_professor)
+);
+
+-- criando tabela aluno
+CREATE TABLE aluno (
+id_aluno INT AUTO_INCREMENT PRIMARY KEY,
+nome_aluno VARCHAR(100) NOT NULL,
+email_aluno VARCHAR(255) NOT NULL UNIQUE,
+senha_aluno VARCHAR(255) NOT NULL,
+id_turma INT NOT NULL,
+FOREIGN KEY (id_turma) REFERENCES turma (id_turma)
+			ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+
+
+-- criando tabela alternativa
+CREATE TABLE alternativa (
+id_alternativa INT AUTO_INCREMENT PRIMARY KEY,
+resposta MEDIUMTEXT NOT NULL,
+tipo_alternativa ENUM('texto', 'imagem', 'mista') NOT NULL,
+correta BOOLEAN NOT NULL DEFAULT 0,
+id_pergunta INT NOT NULL,
+id_imagem INT NULL,
+FOREIGN KEY (id_pergunta) REFERENCES pergunta (id_pergunta)
+			ON DELETE CASCADE ON UPDATE CASCADE,
+FOREIGN KEY (id_imagem) REFERENCES imagem (id_imagem)
+			ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+
+
+-- criando tabela ajuda
+CREATE TABLE ajuda (
+id_ajuda INT AUTO_INCREMENT PRIMARY KEY,
+tipo_ajuda ENUM('eliminar_alternativa', 'dica_textual') NOT NULL,
+conteudo_ajuda MEDIUMTEXT NULL,
+custo_pontos SMALLINT UNSIGNED NOT NULL,
+id_pergunta INT NOT NULL,
+FOREIGN KEY (id_pergunta) REFERENCES pergunta (id_pergunta)
+			ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+
+-- criando tabela partida
+CREATE TABLE partida (
+id_partida INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+id_aluno INT NOT NULL,
+id_sessao INT NOT NULL,
+pontuacao_total INT UNSIGNED NOT NULL DEFAULT 0,
+data_hora_inicio TIMESTAMP NOT NULL DEFAULT current_timestamp,
+data_hora_fim TIMESTAMP NULL,
+status_partida ENUM('em_andamento', 'finalizado', 'ausente') NOT NULL DEFAULT 'em_andamento',
+FOREIGN KEY (id_aluno) REFERENCES aluno (id_aluno)
+			ON UPDATE CASCADE ON DELETE RESTRICT,
+FOREIGN KEY (id_sessao) REFERENCES sessao (id_sessao)
+			ON UPDATE CASCADE ON DELETE RESTRICT
+);
+
+-- criando tabela ajuda_usada
+CREATE TABLE ajuda_usada (
+id_ajuda_usada INT AUTO_INCREMENT PRIMARY KEY,
+id_ajuda INT NOT NULL,
+id_partida INT NOT NULL,
+pontos_descontados SMALLINT UNSIGNED NOT NULL,
+FOREIGN KEY (id_ajuda) REFERENCES ajuda (id_ajuda)
+			ON UPDATE RESTRICT ON DELETE RESTRICT,
+FOREIGN KEY (id_partida) REFERENCES partida (id_partida)
+			ON UPDATE RESTRICT ON DELETE CASCADE
+);
+
+-- criando tabela Resposta Aluno
+CREATE TABLE resposta_aluno(
+id_resposta INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+id_partida INT NOT NULL,
+id_pergunta INT NOT NULL,
+id_alternativa INT NOT NULL,
+pontos_obtidos SMALLINT UNSIGNED NOT NULL,
+data_hora_resposta DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+FOREIGN KEY (id_partida) REFERENCES partida (id_partida)
+			ON UPDATE CASCADE ON DELETE CASCADE,
+FOREIGN KEY (id_pergunta) REFERENCES pergunta (id_pergunta)
+			ON UPDATE CASCADE ON DELETE RESTRICT,
+FOREIGN KEY (id_alternativa) REFERENCES alternativa (id_alternativa)
+			ON UPDATE CASCADE ON DELETE RESTRICT,
+UNIQUE (id_partida, id_pergunta)
+);
+-- criando tabela associativa Perguntas Sessão
+CREATE TABLE perguntas_sessao(
+id_sessao INT NOT NULL,
+id_pergunta INT NOT NULL,
+PRIMARY KEY(id_sessao, id_pergunta),
+FOREIGN KEY (id_sessao) REFERENCES sessao (id_sessao)
+			ON UPDATE CASCADE ON DELETE RESTRICT,
+FOREIGN KEY (id_pergunta) REFERENCES pergunta (id_pergunta)
+			ON UPDATE CASCADE ON DELETE RESTRICT
+);
+
+
+
+
+
+ 
+
---
+++ Banco/02_dados_teste.sql
@@ -0,0 +1,91 @@
+
+USE quiztec;
+
+-- populando a tabela dificuldade
+INSERT INTO dificuldade
+VALUES (default, 'FACIL', 1, 1.0), (default, 'MEDIO', 2, 1.5), (default, 'DIFICIL', 3, 2.0);
+
+
+-- populando tabela professor
+INSERT INTO professor (nome_professor, email_professor, senha_professor)
+VALUES ('João da Silva Júnior', 'joaojunior@etec.sp.gov.br', '$2a$12$MGFOFKGYSF9mRYmqXQP2guSXPYIZnNJN1yw5/WkgDFIm9Lt8SwLd6'),
+('Maria Joana Carvalho', 'mariacarvalho@etec.sp.gov.br', '$2a$12$PA13sgHRBALPIiwrazYTd.Wp1skrgXueZvkh7pezz3Ag/v.Gfdt0W');
+
+
+-- populando tabela turma
+INSERT INTO turma (serie, letra, id_professor)
+VALUES (1, 'A', 1), (1, 'B', 1), (2, 'A', 2);
+
+
+-- populando tabela aluno
+INSERT INTO aluno (nome_aluno, email_aluno, senha_aluno, id_turma)
+VALUES ('Cássio Roberto Ramos', 'cassioramos@aluno.etec.sp.gov.br', '$2a$12$I2INboomLJ.yqTlqmtC4Xulvxzpcifekqe8P9mMbBw9niwHjtFKjK', 1),
+('Yuri Alberto Junior', 'yurialberto@aluno.etec.sp.gov.br', '$2a$12$1rUqzs1druFK7ick070FH.mJA.5z8DrDw0rsfz/eqSLVF6eq5Bcti', 2),
+('Pedro Milans', 'pedromilans@aluno.etec.sp.gov.br', '$2a$12$IVzqyUO7a5fos6QZv/NiT.NwGloL5eOUPCnwRPdlVjFJNX2js/eR2', 3),
+('Breno Bidon da Silva', 'brenobidon@aluno.etec.sp.gov.br', '$2a$12$Dj3v5MQuUV1825G9XeWv6O6S7IoI5ZKITs5SLDnflfTUHlsAn7kgm', 2);
+
+
+
+-- populando a tabela imagem
+INSERT INTO imagem (arquivo_imagem, tipo_imagem, nome_imagem, descricao_imagem)
+VALUES (UNHEX('89504E47'), 'png', 'becker_50ml.png', 'Béquer de vidro com graduação, capacidade de 50ml'),
+(UNHEX('89504E47'), 'png', 'proveta_100ml.png', 'Proveta graduada de 100ml para medição de volumes'),
+(UNHEX('89504E47'), 'png', 'erlenmeyer.png', 'Erlenmeyer de 250ml para reações químicas'),
+(UNHEX('89504E47'), 'png', 'pipeta_volumetrica.png', 'Pipeta volumétrica de 25ml para medições precisas');
+
+
+  
+
+-- populando a tabela pergunta
+INSERT INTO pergunta (enunciado, tipo_pergunta, pontuacao, id_imagem)
+VALUES('Qual o nome desta vidraria?', 'identificacao', 10, 1), ('Para que serve este equipamento?', 'material_funcao', 15, 2),
+('Qual material é usado para medir volumes com precisão?', 'funcao_material', 20, NULL);
+
+
+-- populando tabela alternativa
+INSERT INTO alternativa (resposta, tipo_alternativa, correta, id_pergunta, id_imagem)
+VALUES ('Béquer', 'texto', 1, 1, NULL), ('Proveta', 'texto', 0, 1, NULL), ('Erlenmeyer', 'texto', 0, 1, NULL), ('Balão volumétrico', 'texto', 0, 1, NULL),
+('Medir volumes de líquidos com precisão moderada', 'texto', 1, 2, NULL), ('Aquecer substâncias diretamente na chama do bico de Bunsen', 'texto', 0, 2, NULL),
+('Realizar reações químicas que liberam gases sob agitação', 'texto', 0, 2, NULL), ('Filtrar misturas heterogêneas com auxílio de papel filtro', 'texto', 0, 2, NULL),
+('Pipeta volumétrica', 'imagem', 1, 3, 4), ('Béquer', 'imagem', 0, 3, 1), ('Erlenmeyer', 'imagem', 0, 3, 3), ('Proveta', 'imagem', 0, 3, 2);
+
+
+-- populando tabela ajuda
+INSERT INTO ajuda (tipo_ajuda, conteudo_ajuda, custo_pontos, id_pergunta)
+VALUES ('eliminar_alternativa', NULL, 5, 1), ('dica_textual', 'Observe atentamente o formato deste material. 
+Ele é cilíndrico, alongado e possui marcações numéricas em sua lateral. 
+Equipamentos com graduação servem para realizar mensurações.
+ Pense em qual grandeza física pode ser medida em um recipiente desse formato.', 10, 2);
+
+
+-- populando tabela sessao
+INSERT INTO sessao (nome_sessao, quantidade_perguntas, id_professor, id_dificuldade)
+VALUES ('Vidrarias básicas', 3, 1, 1), ('Sistemas de filtração', 2, 2, 3);
+
+
+-- populando perguntas_sessao
+INSERT INTO perguntas_sessao (id_sessao, id_pergunta)
+VALUES (1, 1), (1, 2), (1, 3), (2, 2), (2, 3);
+
+
+-- populando tabela partida 
+INSERT INTO partida (id_aluno, id_sessao, pontuacao_total, data_hora_fim, status_partida)
+VALUES (1, 1, 25, (current_timestamp + INTERVAL 3 HOUR), 'finalizado'), (2, 2, default, NULL, default), 
+(3, 1, default, (current_timestamp + INTERVAL 10 MINUTE), 'finalizado');
+
+
+
+
+-- populando tabela resposta_aluno 
+INSERT INTO resposta_aluno (id_partida, id_pergunta, id_alternativa, pontos_obtidos)
+VALUES (1, 1, 1, 10), (1, 2, 5, 15), (1, 3, 10, 0), (2, 2, 6, 0), (2, 3, 11, 0), (3, 1, 2, 0), (3, 2, 6, 0), (3, 3, 11, 0);
+
+
+
+
+INSERT INTO ajuda_usada (id_ajuda, id_partida, pontos_descontados)
+VALUES (2, 2, 10);
+
+
+
+
